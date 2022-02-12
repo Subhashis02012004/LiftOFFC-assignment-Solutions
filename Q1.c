4ddb76a5857045ddff28abbdac5cb01e7d298d74
Subhashis02012004 <99485430+Subhashis02012004@users.noreply.github.com>
2022-02-12 20:54:44 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("NAME: SUBHASHIS SATYABRATA BEHERA \n");
+    printf("REGISTRATION NO: 119 \n");
+    printf("BRANCH: CIVIL ENGINEERING\n");
+    printf("HOBBIES: READING BOOKS\n");
+return 0;
+}
+
---
+++ Q2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter any two number: \n");
+    scanf("%d %d", &a ,&b);
+    if(a>b)
+    printf("a is maximum");
+else
+printf("b is maximum");
+return 0;
+}
+
